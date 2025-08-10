e29969bcd5f927c5062e762a00924a796f62dd75
Luben69 <lpacharazov123@gmail.com>
2025-08-10 12:50:50 +0300

    Keep going

---
+++ Arrays_Exercises/magic_sum.js
@@ -0,0 +1,9 @@
+function magic(arr, target) {
+    for (let i = 0; i < arr.length; i ++) {
+        for (let j = i + 1; j < arr.length; j ++) {
+            if (arr[i] + arr[j] === target) {
+                console.log(arr[i], arr[j]);
+            }
+        }
+    }
+}
