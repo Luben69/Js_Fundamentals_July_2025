b9e5c53a8ba12a992910197e5fa4a87e1caa38bc
Luben69 <lpacharazov123@gmail.com>
2025-08-12 11:04:16 +0300

    Going

---
+++ Functions_Lab/orders.js
@@ -0,0 +1,11 @@
+function ordering(product, quantity) {
+    if (product === 'coffee') {
+        console.log((1.50 * quantity).toFixed(2));
+    } else if (product === 'water') {
+        console.log((1.00 * quantity).toFixed(2));
+    } else if (product === 'coke') {
+        console.log((1.40 * quantity).toFixed(2));
+    } else if (product === 'snacks') {
+        console.log((2.00 * quantity).toFixed(2));
+    }
+}
