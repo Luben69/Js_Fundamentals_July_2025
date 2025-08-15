d9799e7628d70ad95fedafd57562eb928e84d714
Luben69 <lpacharazov123@gmail.com>
2025-08-15 16:40:34 +0300

    Keep it up

---
+++ Functions_More_Exercises/car_wash.js
@@ -0,0 +1,18 @@
+function washing(arr) {
+    let clean = 0;
+
+    for (let command of arr) {
+        if (command === 'soap') {
+            clean += 10;
+        } else if (command === 'water') {
+            clean += clean * 0.2;
+        } else if (command === 'vacuum cleaner') {
+            clean += clean * 0.25;
+        } else if (command === 'mud') {
+            clean -= clean * 0.1;
+        }
+    }
+    console.log(`The car is ${clean.toFixed(2)}% clean.`);
+}
+
+washing(['soap', 'soap', 'vacuum cleaner', 'mud', 'soap', 'water']);
