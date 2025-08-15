c19f7f2766f8ea29ca039c859c7021595c1393e2
Luben69 <lpacharazov123@gmail.com>
2025-08-15 17:33:47 +0300

    Keep it up

---
+++ Functions_More_Exercises/points_validation.js
@@ -0,0 +1,14 @@
+function validateDistances(points) {
+    let [x1, y1, x2, y2] = points;
+
+    function isValidDistance(x1, y1, x2, y2) {
+        let distance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+        return distance % 1 === 0;
+    }
+
+    console.log(`{${x1}, ${y1}} to {0, 0} is ${isValidDistance(x1, y1, 0, 0) ? 'valid' : 'invalid'}`);
+
+    console.log(`{${x2}, ${y2}} to {0, 0} is ${isValidDistance(x2, y2, 0, 0) ? 'valid' : 'invalid'}`);
+
+    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${isValidDistance(x1, y1, x2, y2) ? 'valid' : 'invalid'}`);
+}
