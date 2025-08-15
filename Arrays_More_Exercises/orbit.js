41b286b34935bc7ec68dd91567d319892779b195
Luben69 <lpacharazov123@gmail.com>
2025-08-15 11:28:59 +0300

    Lets go

---
+++ Arrays_More_Exercises/orbit.js
@@ -0,0 +1,22 @@
+function orbiting(dimensions) {
+    let width = dimensions.shift();
+    let height = dimensions.shift();
+    let x = dimensions.shift();
+    let y = dimensions.shift();
+    let matrix = [];
+
+    for (let i = 0; i < width; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < height; j++) {
+            let distance = Math.max(Math.abs(i - x), Math.abs(j - y)) + 1;
+            matrix[i][j] = distance;
+        }
+    }
+
+    for (let row = 0; row < width; row++) {
+        console.log(matrix[row].join(' '));
+    }
+}
+
+orbiting([5, 5, 2, 2]);
+orbiting([4, 4, 0, 0]);
