52af70389d59ef088b3f11bea7eee6a1aa15f21a
Luben69 <lpacharazov123@gmail.com>
2025-08-15 13:18:35 +0300

    Going

---
+++ Functions_Exercises/nxn_matrix.js
@@ -0,0 +1,9 @@
+function matrix(n) {
+    for (let i = 0; i < n; i ++) {
+        let result = [];
+        for (let j = 0; j < n; j ++) {
+            result.push(n);
+        }
+        console.log(result.join(' '));
+    }
+}
